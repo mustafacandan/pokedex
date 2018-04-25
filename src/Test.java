4984f9b7d4bf4d12291b92c13c99ea89928db454
Denizhan Aksakal <denizhanaksakal@hotmail.com>
2018-04-25 21:45:49 +0300

    kontrol 1

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,33 @@
 
 public class Test {
 	public static void main(String[] args) {
-	
+
 	}
-	
-	public class Node {
-		private String item;
-		private int count;
-		private Node link;
-		public Node( )
+
+	public class DoublyLinkedList 
+	{	
+		private Node first;
+		private Node last;
+	}	
+	public DoublyLinkedList( )
+	{
+		first = null;
+		last = null;
+	}
+	public boolean isEmpty()
+	{
+		return (first == null);
+	}
+	public void insertAtFront(String itemName)
+	{
+		if(isEmpty())
+			first = last = new Node(itemName, first);
+		else
 		{
-		link = null;
-		item = null;
-		count = 0;
+			Node a = new Node(itemName);
+			a.setNext(first);
+			first.setPrev(a);
+			first = a;
 		}
 	}
 }
