17fa3370188d80e89a83b42a5501de7764e4a908
Denizhan Aksakal <denizhanaksakal@hotmail.com>
2018-04-25 21:17:05 +0300

    Initial Commit

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+	public static void main(String[] args) {
+	
+	}
+	
+	public class Node {
+		private String item;
+		private int count;
+		private Node link;
+		public Node( )
+		{
+		link = null;
+		item = null;
+		count = 0;
+		}
+	}
+}
